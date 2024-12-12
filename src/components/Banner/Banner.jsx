07ecee01070ba6cd46d62adf5ffe9f961ee60f8e
Plamedi-Pindi <plamedipindi@gmail.com>
2024-12-12 23:27:12 +0100

    Fixing background

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@
 export default function Banner() {
   return (
     <div className=" w-full pr-5 pl-4 ">
-        <div className="h-48 w-full rounded-xl bg-[url('3.webp')] bg-center bg-cover p-2  text-[#7b4c2d] flex justify-center items-center  ">
+        <div className="h-48 w-full rounded-xl bg-[url('/3.webp')] bg-center bg-cover p-2  text-[#7b4c2d] flex justify-center items-center  ">
           <div className="bg-[#d7e6e6]/90 p-2 h-[80%] w-[90%] rounded-lg ">
 
             <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, quam?</p>
